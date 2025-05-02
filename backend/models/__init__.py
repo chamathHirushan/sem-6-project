e65548057220f1f142b5b9f35e428936d60bfece
chamathHirushan <chamathg.21@cse.mrt.ac.lk>
2025-05-02 16:56:36 +0530

    fix: dockerizatin expose frontend

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -4,4 +4,3 @@ from .job import JobPost, JobApplication, Working
 from .service import ServiceCategory, ServicePost
 from .comment import CommentReview, Reply
 from .worker import WorkerProfile, WorkerPost
-
