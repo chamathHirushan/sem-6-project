2e9921fb5ea4010c8184b24d01445ab4e0bc7f4f
Induwara <induwaragunasena.sl@gmail.com>
2025-05-17 17:05:03 +0530

    feat: Beautify the tiles and added neccasory fields

--- frontend/src/components/TaskTile/TaskTile.tsx
+++ frontend/src/components/TaskTile/TaskTile.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import {ClockIcon, MapPinIcon, TagIcon, StarIcon, ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/outline";
+import { StarIcon as SolidStarIcon } from "@heroicons/react/24/solid";
 import "./TaskTile.css";
 
 interface TaskTileProps {
   id: string;
   image: string;
-  address: string;
+  title: string;
+  location: string;
   daysPosted: number;
   taskType: string;
+  budget: string;   // budget can be a string(for negotiable services) or number
   isBookmarked: boolean;
   view: "grid" | "list"; // Determines the tile layout
   onBookmarkToggle: (id: string) => void;
@@ -16,14 +20,16 @@ interface TaskTileProps {
 const TaskTile: FC<TaskTileProps> = ({
   id,
   image,
-  address,
+  title,
+  location,
   daysPosted,
   taskType,
   isBookmarked,
+  budget, 
   view,
   onBookmarkToggle,
 }) => {
-  const location = useLocation();
+  const pageLocation = useLocation();
   const navigate = useNavigate();
 
   const handleClick = (e: React.MouseEvent) => {
@@ -34,7 +40,7 @@ const TaskTile: FC<TaskTileProps> = ({
     // Navigate manually with scroll position in state
     navigate(`/hire/${id}`, {
       state: {
-        from: location,
+        from: pageLocation,
         scrollPosition: scrollY,
       },
     });
@@ -46,44 +52,123 @@ const TaskTile: FC<TaskTileProps> = ({
         <>
           <img src={image} alt="Task" className="task-tile-image" />
           <div className="task-tile-content">
-            <h3 className="task-tile-id">Job ID: {id}</h3>
-            <p className="task-tile-address">{address}</p>
-            <button
-              className={`task-tile-bookmark ${isBookmarked ? "bookmarked" : ""}`}
-              onClick={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-                onBookmarkToggle(id);
-              }}
-            >
-              {isBookmarked ? "Unbookmark" : "Bookmark"}
-            </button>
+            <div className="task-tile-title-wrapper" title={title}>
+              <h3 className="task-tile-title truncate">{title}</h3>
+            </div>
+            <h3 className="task-tile-id">Rs. {budget}</h3>
+            
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-0.5">
+                {/* Task type */}
+                <div className="flex items-center">
+                  <TagIcon className="w-4 h-4 mr-1" style={{ color: "red" }} />
+                  <p className="task-tile-type">{taskType}</p>
+                </div>
+
+                {/* Location */}
+                <div className="flex items-center">
+                  <MapPinIcon className="w-4 h-4 mr-1" style={{ color: "#055dff" }} />
+                  <p className="task-tile-address">{location}</p>
+                </div>
+
+                {/* Posted time */}
+                <div className="flex items-center">
+                  <ClockIcon className="w-4 h-4 mr-1" />
+                  <p className="task-tile-days">{daysPosted} days ago</p>
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-1">
+                {/* Chat icon */}
+                <button
+                  className="task-tile-chat"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    navigate(`/chat/${id}`);
+                  }}
+                >
+                  <ChatBubbleOvalLeftEllipsisIcon className="w-5 h-5" />
+                </button>
+
+                {/* Bookmark icon */}
+                <button
+                className={`task-tile-bookmark ${isBookmarked ? "bookmarked" : ""}`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  onBookmarkToggle(id);
+                }}
+                >
+                {isBookmarked ? (
+                  <SolidStarIcon className="w-5 h-5" />
+                ) : (
+                  <StarIcon className="w-5 h-5" />
+                )}
+                </button>
+              </div>
+            </div>
+            
           </div>
         </>
       ) : (
         <>
-          <div className="image-wrapper w-1/3 bg-gray-200 flex items-center justify-center">
+          <div className="image-wrapper w-1/3 bg-gray-200 flex items-center justify-center shrink-0">
             <div className="image-square aspect-square w-full h-full flex items-center justify-center">
               <img src={image} alt="Job" className="max-w-full max-h-full object-contain" />
             </div>
           </div>
 
           <div className="task-tile-content flex-grow">
-            <h2 className="task-tile-title">Job Title</h2>
-            <h3 className="task-tile-id">Job ID: {id}</h3>
+            <h2 className="line-clamp-2 text-neutral-700 font-medium text-lg" title={title}>{title}</h2>
+            <h3 className="task-tile-id">Rs. {budget}</h3>
             <p className="task-tile-type">Type: {taskType}</p>
-            <p className="task-tile-days">{daysPosted} days ago</p>
-            <p className="task-tile-address">{address}</p>
-            <button
-              className={`task-tile-bookmark ${isBookmarked ? "bookmarked" : ""}`}
-              onClick={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-                onBookmarkToggle(id);
-              }}
-            >
-              {isBookmarked ? "Unbookmark" : "Bookmark"}
-            </button>
+           
+           {/* location */}
+            <div className="flex items-center">
+              <MapPinIcon className="w-4 h-4 mr-1" style={{ color: "#055dff" }}/>
+              <p className="task-tile-address">{location}</p>
+            </div>
+
+            <div className="flex items-center justify-between w-full">
+              <div>
+                {/* Bookmark icon */}
+                <button
+                className={`task-tile-bookmark ${isBookmarked ? "bookmarked" : ""}`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  onBookmarkToggle(id);
+                }}
+                >
+                {isBookmarked ? (
+                  <SolidStarIcon className="w-5 h-5" />
+                ) : (
+                  <StarIcon className="w-5 h-5" />
+                )}
+                </button>
+                
+                {/* chat icon */}
+                <button
+                className="task-tile-chat"
+                onClick={(e) => { 
+                  e.stopPropagation();
+                  e.preventDefault();
+                  navigate(`/chat/${id}`);
+                }}
+                > 
+                <ChatBubbleOvalLeftEllipsisIcon className="w-5 h-5" />
+                </button>
+
+              </div>
+              
+              
+              {/* posted time */}
+              <div className="flex items-center">
+                <ClockIcon className="w-4 h-4 mr-1"/>
+                <p className="task-tile-days">{daysPosted} days ago</p>
+              </div>
+            </div>
           </div>
         </>
       )}
